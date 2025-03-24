598827e598dc22e6c889b1eac36691104b09625e
Radosław Grzanka <radoslawg@gmail.com>
2025-03-24 21:23:04 +0100

    ✨ Enhance player movement and animations
    - Added JumpSpeed property to control jump height.
    - Implemented gravity effect when the player is in the air.
    - Updated player sprite scaling based on movement direction.
    - Integrated jump animation for both upper and lower body during jump.
    - Refined animation playback for running and idle states.

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -10,15 +10,20 @@ public partial class Player : CharacterBody2D
 {
     private AnimationPlayer _animationPlayerUpper;
     private AnimationPlayer _animationPlayerLower;
+    private Node2D _sprite;
 
     [Export]
     public int Speed { get; set; } = 50;
+
+    [Export]
+    public int JumpSpeed { get; set; } = 200;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _animationPlayerUpper = GetNode<AnimationPlayer>("AnimationPlayerUpper");
         _animationPlayerLower = GetNode<AnimationPlayer>("AnimationPlayerLower");
+        _sprite = GetNode<Node2D>("Sprite");
         base._Ready();
     }
 
@@ -31,16 +36,43 @@ public partial class Player : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         float direction = Input.GetAxis("ui_left", "ui_right");
-        Velocity = Vector2.Right * direction * Speed;
+
+        if (!IsOnFloor())
+        {
+            Velocity += Vector2.Down * GetGravity() * (float)delta;
+        }
+
+        Velocity = Velocity with { X = direction * Speed };
+
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+        {
+            Velocity = Velocity with { Y = -JumpSpeed };
+        }
+
+        if (Velocity.X < 0)
+        {
+            _sprite.Scale = _sprite.Scale with { X = -1 };
+        }
+        else if (Velocity.X > 0)
+        {
+            _sprite.Scale = _sprite.Scale with { X = 1 };
+        }
+
         if (direction != 0)
         {
-            _animationPlayerUpper.Play("run");
             _animationPlayerLower.Play("run");
+            _animationPlayerUpper.Play("run");
         }
         else
         {
-            _animationPlayerUpper.Play("idle");
             _animationPlayerLower.Play("idle");
+            _animationPlayerUpper.Play("idle");
+        }
+
+        if (!IsOnFloor())
+        {
+            _animationPlayerLower.Play("jump");
+            _animationPlayerUpper.Play("jump");
         }
 
         MoveAndSlide();
